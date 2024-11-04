6cac8438bea8600fe542b6b07f22127bebcdade0
AndreLuisPLuz <andreluispluz@gmail.com>
2024-11-04 09:18:02 -0300

    build: new npm build script, environment variables and db connection changed.

--- src/infrastructure/sources/mongoSource.ts
+++ src/infrastructure/sources/mongoSource.ts
@@ -5,8 +5,11 @@ const getMongoSource = () => {
     const host = process.env.DB_HOST;
     const port = process.env.DB_PORT;
     const name = process.env.DB_NAME;
+    const user = process.env.DB_USER;
+    const pass = process.env.DB_PASS;
 
-    const dbString = `mongodb://${host}:${port}/${name}`;
+    const dbString = `mongodb+srv://${user}:${pass}@${host}`;
+    // const dbString = `mongodb://${host}:${port}/${name}`;
     mongoose.connect(dbString, {})
         .then(() => console.log(`[server]: connected to ${dbString}`));
 }
